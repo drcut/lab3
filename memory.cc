bbf423767429130107f1bed0af03d7a04cc3d98a
Baidi96 <1400012861@pku.edu.cn>
2016-11-26 00:05:16 +0800

    update print

--- memory.cc
+++ memory.cc
@@ -9,6 +9,7 @@ void Memory::HandleRequest(uint64_t addr, int bytes, int read,
 	hit = 1;
 	time = latency_.hit_latency + latency_.bus_latency;
 	stats_.access_time += time;
+	stats_.access_counter++;
   
 #ifdef PROG_SIM
 	if(read)
