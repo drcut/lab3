c8c72ba9fd5d88f4f92e05d764f02d45eb200d61
Baidi96 <1400012861@pku.edu.cn>
2016-11-25 23:49:58 +0800

    update print

--- memory.cc
+++ memory.cc
@@ -5,5 +5,6 @@ void Memory::HandleRequest(uint64_t addr, int bytes, int read,
   hit = 1;
   time = latency_.hit_latency + latency_.bus_latency;
   stats_.access_time += time;
+  stats_.access_counter++;
 }
 
